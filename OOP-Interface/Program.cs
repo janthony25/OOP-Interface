152d82a45fbb90640e23d322b9ff9327b9ac8da4
janthony25 <junanthony.business@gmail.com>
2024-02-04 13:02:39 +1300

    Add project files.

---
+++ OOP-Interface/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // interface = defines a "contract" that all the classes inheriting from should follow
+
+            //             An interface declares "what a class should have"
+            //             An inheriting class defines "how it should do it"
+
+            //             Benefit = security + multiplle inheritance + "plug-and-play"
+
+            Rabbit rabbit = new Rabbit();
+            Hawk hawk = new Hawk();
+            Fish fish = new Fish();
+
+            // rabbit.Flee();
+            // hawk.Hunt();
+            fish.Flee();
+            fish.Hunt();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IPrey
+    {
+        void Flee();
+    }
+
+    interface IPredator
+    {
+        void Hunt();
+    }
+
+    class Rabbit : IPrey
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The Rabbit runs away!");
+        }
+    }
+
+     class Hawk : IPredator
+    {
+        public void Hunt()
+        {
+            Console.WriteLine("The Hawk is search for its prey");
+        }
+    }
+
+    class Fish : IPrey, IPredator
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The fish swims away");
+        }
+
+        public void Hunt()
+        {
+            Console.WriteLine("The fish is looking for a smaller fish to eat");
+        }
+    }
+}
